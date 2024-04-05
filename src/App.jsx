5d67586d4501ac501eade3e4daa9a0d460e45d82
UmarAhmad12 <umara6274@gmail.com>
2024-04-06 04:45:27 +0530

    Added 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import AboutUs from "./Pages/AboutUs";
 import HomePage from "./Pages/HomePage";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
+      
+      
+      
+      <Route path="*" element={<NotFound/>}/>
       </Routes>
     </>
   );
