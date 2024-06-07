6ad7e1a1c2006345f7ea5792eda38f8f72d9b24d
UmarAhmad12 <umara6274@gmail.com>
2024-06-07 21:15:01 +0530

    Added course lectures

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        primary: '#3490dc', // Define your primary color
-        secondary: '#e06aef', // Define your secondary color
-      },
+      // colors: {
+      //   primary: '#3490dc', // Define your primary color
+      //   secondary: '#e06aef', // Define your secondary color
+      // },
     },
   },
   plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
